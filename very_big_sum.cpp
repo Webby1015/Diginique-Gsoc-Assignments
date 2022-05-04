54b52e880c30d6a3f3313114238a7de5dddd56f6
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-05-04 08:59:08 +0530

    Create very_big_sum.cpp

---
+++ very_big_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    long a[n];
+    long sum = 0;
+    for(int i = 0; i<n;i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i<n;i++){
+        sum = sum +a[i];
+    }
+    cout<<sum;
+}
