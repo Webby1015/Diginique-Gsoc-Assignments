1b9f568b3d8287498ca3e05f90d021f5454b6051
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-05-06 00:44:21 +0530

    Create count primes.cpp

---
+++ Assignment3/count primes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+	for (int i=2;i<=sqrt(n);i++)
+	{
+		if(n%i==0){
+			return 0;
+	}}
+	return 1;
+}
+int main(){
+	int s,c=0,l;
+	cin>>s;
+	for(int i=2;i<s;i++){
+		l=isprime(i);
+		if(l==1){
+			c++;
+		}}
+	cout<<c;
+}
