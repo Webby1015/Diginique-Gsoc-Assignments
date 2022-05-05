7a8e69863bbf8b6d94a85440085a824eae4142db
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-05-06 00:51:21 +0530

    Update Code to test getTwoLargestNumbers.cpp

--- Assignment2/Code to test getTwoLargestNumbers.cpp
+++ Assignment2/Code to test getTwoLargestNumbers.cpp
@@ -5,17 +5,17 @@ int passedTestCase=0; //global variable
 
 vector<int>getTwoLargestNumbers(vector<int>&A){
     int max1=INT_MIN, max2=INT_MIN;
-    for(int i=0;i<A.size();i++){
-        if(max1<A[i]){
+    for(int i=0;i<10;i++){
+        if(max1<=A[i]){
+        max2=max1;
         max1=A[i];
         }
     }
-    for(int i=0;i<A.size();i++){
-        if(max2<A[i] && A[i]<max1){
+    for(int i=0;i<10;i++){
+        if(A[i]>max2 && A[i]!=max1){
         max2=A[i];
         }
     }
-    
     
     return {max1,max2};
 }
@@ -54,5 +54,3 @@ int main() {
     }
     cout<<"Test Case Passed: "<<passedTestCase<<"\n";
 }
-
-
