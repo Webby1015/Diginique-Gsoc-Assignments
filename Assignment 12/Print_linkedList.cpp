7e895ee1c7b99335f3fbf438946241415efd949b
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-06-07 03:56:11 +0530

    Create Print_linkedList.cpp

---
+++ Assignment 12/Print_linkedList.cpp
@@ -0,0 +1,18 @@
+
+
+
+void printLinkedList(SinglyLinkedListNode* head) {
+    if(head==NULL){
+        return ;
+    }
+    else{
+        SinglyLinkedListNode*p;
+        p=head;
+        while(p!=NULL){
+            cout<<p->data<<"\n";
+            p=p->next;
+        }
+    }
+
+
+}
