60e71ef13c0b685a41c2c81e8c005caeb5515f80
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-06-09 14:04:18 +0530

    Create First_insert_linkedlist.cpp

---
+++ Misc Programs/First_insert_linkedlist.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node * link;
+};
+
+void printList(Node * p){
+    while(p != NULL){
+        cout<<p->data<<endl;
+        p=p->link;
+    }
+}
+void insert(Node * i){
+    Node * a = new Node;
+    cout<<"Insert element :"<<endl;
+    cin>>a->data;
+    a->link=i;
+    printList(a);
+}
+
+
+
+int main(){
+    Node * head = new Node;
+    Node * a = new Node;
+    Node * b = new Node;
+    Node * c = new Node;
+    
+    head->link=a;
+    a->data = 1;
+    a->link = b;
+    b->data = 2;
+    b->link = c;
+    c->data = 3;
+    c->link = NULL;
+    insert(a);
+    
+}
