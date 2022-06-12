6d7df858ffbe0db8edcc6c0cec843185c1b80b60
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-06-12 06:53:32 +0530

    Create Arranging_coins.cpp

---
+++ Assignment 10/Arranging_coins.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long start = 0;
+        long end = n;
+        long ans;
+        
+        while(start <= end) {
+            ans = start + (end-start)/2;
+            if(ans*(ans+1)/2 == n) return (int)ans;
+            else if(ans*(ans+1)/2 < n) start = ans+1;
+            else end = ans-1;
+        }
+        return (int)end;
+    }
+};
