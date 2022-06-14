2e4ab2ed67260758257430cabb65ed21d9ad9263
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-06-15 03:35:13 +0530

    Update prime_number_in_given_range.cpp

--- Assignment2/prime_number_in_given_range.cpp
+++ Assignment2/prime_number_in_given_range.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 bool isPrime(int n){
-   int c=1;
-   for(int i=2;i<=sqrt(n);i++)
-   {
-      if(n%i == 0)
-      {
-         c = 0;
-         break;
-      }
-   }
-    return c;
+    if (n <= 1)
+        return 0;
+ 
+    for (int i = 2; i <= sqrt(n); i++){
+        cout<<n<<" "<<sqrt(n)<<endl;
+        if (n % i == 0)
+            return 0;
+    }
+    
+    return 1;
 }
 int main() {
     for(int i=1;i<30;i++){
