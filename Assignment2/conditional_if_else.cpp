6d0b556ca5e5dfb2d60e3d715764a73cf3106363
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-06-14 23:16:56 +0530

    Update conditional_if_else.cpp

--- Assignment2/conditional_if_else.cpp
+++ Assignment2/conditional_if_else.cpp
@@ -6,38 +6,7 @@ int main()
 {
     int n;
     cin>>n;
-    
-        if(n==1){
-            cout<<"one";
-        }
-        else if(n==2){
-            cout<<"two";
-        }
-        else if(n==3){
-            cout<<"three";
-        }
-        else if(n==4){
-            cout<<"four";
-        }
-        else if(n==5){
-            cout<<"five";
-        }
-        else if(n==6){
-            cout<<"six";
-        }
-        else if(n==7){
-            cout<<"seven";
-        }
-        else if(n==8){
-            cout<<"eight";
-        }
-        else if(n==9){
-            cout<<"nine";
-        }
-        
-        else
-        {
-            cout<<"Greater than 9";
-        }
-    
+    vector<string>a={"Greater than 9","one","two","three","four","five","six","seven","eight","nine"};
+    if(n<=9 && n>0) cout<<a[n];
+    else cout<<a[0];
 }
