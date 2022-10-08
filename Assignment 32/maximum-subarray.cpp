8d69ca7f78339e7cef788ea2ab924a5f14c1ea7b
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2022-10-08 15:25:53 +0530

    Create maximum-subarray.cpp

---
+++ Assignment 32/maximum-subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = size(nums), ans = INT_MIN;
+        for(int i = 0; i < n; i++) 
+            for(int j = i, curSum = 0; j < n ; j++) 
+                curSum += nums[j],
+                ans = max(ans, curSum);        
+        return ans;
+    }
+};
